e381ff93975b9fa5f2b5c3fe37198f5405eb0afe
unknown <blooodcf@gmail.com>
2019-07-28 00:05:36 -0300

    Relationship UserBook

--- backend/src/model/UserBook.js
+++ backend/src/model/UserBook.js
@@ -1,13 +1,26 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/database");
 
+const Book = require("./Book");
+const User = require("./User");
+
 const UserBook = sequelize.define("UsuariosLivros", {
   id_usuario: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    references: {
+      model: "usuarios",
+      key: "id_usuario"
+    }
   },
   id_livro: {
-    type: DataTypes.INTEGER
+    type: DataTypes.INTEGER,
+    references: {
+      model: "livros",
+      key: "id_livro"
+    }
   }
 });
+User.belongsToMany(Book, { through: UserBook, foreignKey: "id_usuario" });
+Book.belongsToMany(User, { through: UserBook, foreignKey: "id_livro" });
 
 module.exports = UserBook;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const app = require("express")();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const User = require("./model/User");
+const Book = require("./model/Book");
+const UserBook = require("./model/UserBook");
 
 // Express middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,6 +18,18 @@ const userRoute = require("./routes/user");
 // app.use("/auth", authRoute);
 // app.use("/test", testRoute);
 app.use("/user", userRoute);
+
+UserBook.create({
+  id_usuario: 1,
+  id_livro: 1
+});
+
+// Create Book
+// Book.create({
+//   nome: "Anjos e demonios",
+//   autor: "Dan Brown",
+//   genero: "Suspense"
+// });
 
 // Create user
 // User.create({
