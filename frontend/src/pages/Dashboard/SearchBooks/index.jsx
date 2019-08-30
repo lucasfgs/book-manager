7848043e33342a2fecb13b3c0afe72b56c73c3f2
unknown <blooodcf@gmail.com>
2019-08-30 02:25:34 -0300

    SearchBooks page input field

--- frontend/src/pages/Dashboard/SearchBooks/index.jsx
+++ frontend/src/pages/Dashboard/SearchBooks/index.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { MdSearch } from "react-icons/md";
 
-import { Container } from "./styles.js";
+import { Container, Search } from "./styles.js";
 
-const SearchBooks = () => <Container />;
+const SearchBooks = () => {
+  return (
+    <Container>
+      <Search>
+        <input type="text" placeholder="Insira o livro desejado" />
+        <button>
+          <MdSearch color="#ffaa00" size={25} />
+        </button>
+      </Search>
+    </Container>
+  );
+};
 
 export default SearchBooks;
--- frontend/src/pages/Dashboard/SearchBooks/styles.js
+++ frontend/src/pages/Dashboard/SearchBooks/styles.js
@@ -1,7 +1,34 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  display: flex;
   height: 100vh;
   width: 100vw;
-  background: #000;
+  background: #2f3542;
+`;
+
+export const Search = styled.div`
+  display: flex;
+
+  margin: 20px auto;
+
+  input {
+    background-color: transparent;
+    outline: 0;
+    border: 2px solid #ffaa00;
+    width: 300px;
+    height: 40px;
+    padding: 10px;
+    font-size: 15px;
+    color: #dcdde1;
+  }
+
+  button {
+    background-color: transparent;
+    border: 2px solid #ffaa00;
+    border-left: 0;
+    height: 40px;
+    width: 50px;
+    cursor: pointer;
+  }
 `;
